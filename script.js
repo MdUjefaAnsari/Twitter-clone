8416ceb664938a2c3f203a7972031db4c0bc55d3
MdUjefaAnsari <mdujefaansari@gmail.com>
2023-10-06 15:32:38 +0530

    added js functionality

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const toggleButton = document.getElementById('toggleButton');
-const iconsContainer = document.querySelector('.icons');
+const followButton = document.querySelectorAll(".btn");
+let isGood = false;
 
-toggleButton.addEventListener('click', () => {
-    iconsContainer.classList.toggle('hide-icons');
+followButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (isGood) {
+      button.textContent = "Follow";
+      button.style.backgroundColor = ""; // Set background color to grey
+      button.style.color= "";
+      button.style.outline="none";
+    } else {
+      button.textContent = "Following";
+      button.style.backgroundColor = "whitesmoke"; // Set background color to green
+      button.style.color= "black";
+      button.style.outline="1px solid grey"
+
+    }
+
+    isGood = !isGood;
+  });
 });
+
